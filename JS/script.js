65bb20d765aa9aa7d29cf07203cb8af670231ae2
joaov-Antunes <joaovicenteftn@gmail.com>
2022-05-12 08:56:35 -0300

    apis conectadas

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,10 @@
+
 function darkTheme() {
     document.body.classList.toggle('dark')
     document.querySelector('.top').classList.toggle('dark')
     document.querySelector('.sidebar').classList.toggle('dark')
     document.querySelector('.bottom').classList.toggle('dark')
+    document.querySelector('.menu-btn').classList.toggle('dark')
 
-    document.querySelector('#theme').innerHTML = '<a href="#" id="theme" onclick="darkTheme()"><ion-icon name="sunny-outline" class="theme-btn"></ion-icon></a>'
+    document.querySelector('.theme-btn').classList.toggle('dark')
 }
