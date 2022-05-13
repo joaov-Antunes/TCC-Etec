fa918fdb249cf49b3a9eab84535908d5cba7f5d6
joaov-Antunes <joaovicenteftn@gmail.com>
2022-05-13 10:28:57 -0300

    funcionalidades finais

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,10 @@
-let aside = document.querySelector('.sidebar').classList.toggle('dark')
-
 function darkTheme() {
     document.body.classList.toggle('dark')
     document.querySelector('.top').classList.toggle('dark')
-    document.querySelector('iframe').classList.toggle('dark')
     document.querySelector('.sidebar').classList.toggle('dark')
     document.querySelector('.bottom').classList.toggle('dark')
     document.querySelector('.menu-btn').classList.toggle('dark')
-    document.querySelector('.container').classList.toggle('dark')
-
     document.querySelector('.theme-btn').classList.toggle('dark')
+    document.querySelector('.map-card').classList.toggle('dark')
+    document.querySelector('.container').classList.toggle('dark')
 }
